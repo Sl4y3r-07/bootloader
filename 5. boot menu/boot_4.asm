86810c36e90f7c9d6d0ec0ace8b2bf01749e8b52
Sl4y3r-07 <ayush_g@ee.iitr.ac.in>
2024-06-02 23:13:42 +0530

    updated

--- 5. boot menu/boot_4.asm
+++ 5. boot menu/boot_4.asm
@@ -12,7 +12,7 @@ org 0x7c00
   ; for Boot Menu 
     mov si, message
     mov cx, message_len
-    mov dh, 0x1 ; setting row for center pos
+    mov dh, 0x1  ; setting row for center pos
     mov dl, 0x2  ; setting column for center pos
     mov ah, 0x02 ; for setting the cursor position
     xor bh,bh    ; default page number, 0
@@ -66,12 +66,12 @@ print_3:
     inc si             ; Move to the next character
     loop print_3
     
-    mov dl, 27
+    mov dl, 28
     mov ah, 0x02 ; for setting the cursor position
     mov bh,0    ; default page number, 0
     int 0x10
     
-
+    xor di,di
 ; for input
 input:
     mov ax,0x00             ; get keyboard input
@@ -84,7 +84,9 @@ input:
     je skip_input       ; Skip handling for special keys
     cmp al, 0xE0        ; Check for extended key (e.g., arrow keys)
     je skip_input       ; Skip handling for extended keys
-    inc dl              ; If backspace, handle deletion
+    inc dl  
+    mov [input_buffer + di], al
+    inc di            ; 
     mov ah,0x0E         ; display input char
     int 0x10
     jmp input
@@ -95,7 +97,7 @@ skip_input:
 
 
 backspace:
-    cmp dl, 27         ; Check if cursor is at starting column
+    cmp dl, 28        ; Check if cursor is at starting column
     jl input           ; If at starting column, do nothing
     mov ah, 0x0E       ; 
     mov al, 0x20       ; 
@@ -107,18 +109,56 @@ backspace:
     jmp input
 
 enter:
+     ;mov byte [input_buffer + di], 0
+    mov si, input_buffer
+    call check_input
+    hlt
+
+check_input:
     inc dh
-    mov dl, 0x2
-    mov ah, 0x02
-    xor bh,bh        
-    int 0x10
-    jmp $
+    mov dl, 0x2  ; setting column for center pos
+    mov ah, 0x02 ; for setting the cursor position
+    xor bh,bh    ; default page number, 0
+    int 0x10     ; BIOS video interrupt
+    cmp byte [si], '1'
+    je normal_mode
+    cmp byte [si], '2'
+    je recovery_mode
+
+    ; If input is neither 1 nor 2, display error message
+    mov si, wrong_choice_msg
+    call print_string
+    jmp hehe
+
+normal_mode:
+   
+    mov si, normal_msg
+    call print_string
+    jmp hehe
+
+recovery_mode:
+    mov si, recovery_msg
+    call print_string
+    jmp hehe
+
+
+
+print_string:
+    mov ah, 0x0E           ; BIOS teletype function
+.print_char:
+    lodsb                  ; Load next byte from string into AL
+    cmp al, 0
+    je .done               ; If null-terminator, we're done
+    int 0x10               ; BIOS video interrupt
+    jmp .print_char
+.done:
+    ret
 
 
 message db 'Boot Options', 0
 message_len equ $ - message
 
-message_1 db '1. Normal Mode', 0
+message_1 db '1. Normal Mode [Default mode]', 0
 message_len_1 equ $ - message_1
 
 message_2 db '2. Recovery Mode', 0
@@ -127,7 +167,14 @@ message_len_2 equ $ - message_2
 message_3 db 'Select Your Choice (1/2) ', 0
 message_len_3 equ $ - message_3
 
-; jmp $
+wrong_choice_msg db 'Wrong choice', 0
+normal_msg db 'normal',0
+normal_msg_len equ $-normal_msg
+recovery_msg db 'recovery',0
+;wrong_choice_msg_len equ $- wrong_choice_msg
+
+input_buffer times 1 db 0
+ hehe: jmp $
 times 510-($-$$) db 0 ; rest bytes with 0
 dw 0xaa55   ; boot sector signature/ magic number
 
